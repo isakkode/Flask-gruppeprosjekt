a21ac0ac0df4bf2e196c3ab0ab643ecfcf387c70
Ostepopp7 <isakridinger@outlook.com>
2024-10-24 11:22:09 +0200

    created all the endspoints and routes

--- project/controllers/hello.py
+++ project/controllers/hello.py
@@ -1,7 +1,39 @@
 from project import app
 from flask import render_template, request, redirect, url_for
 
-#route index
+
 @app.route('/', methods=["GET", "POST"])
 def index():
-    render_template('index.html.j2')
+    return render_template('index.html.j2')
+
+
+@app.route('/order-car.html.j2', methods=["GET", "POST"])
+def order_car():
+    if request.method == "POST":
+        result = request.form.to_dict()
+        print(result)
+    return render_template('order-car.html.j2')
+
+
+@app.route('/cancel-order-car.html.j2', methods=["GET", "POST"])
+def return_order_car():
+    if request.method == "POST":
+        result = request.form.to_dict()
+        
+    return render_template('cancel-order-car.html.j2')
+
+
+@app.route('/rent-car.html.j2', methods=["GET", "POST"])
+def rent_car():
+    if request.method == "POST":
+        result = request.form.to_dict()
+        print(result)
+    return render_template('rent-car.html.j2')
+
+
+@app.route('/return-car.html.j2', methods=["GET", "POST"])
+def return_car():
+    if request.method == "POST":
+        result = request.form.to_dict()
+        print(result)
+    return render_template('return-car.html.j2')
