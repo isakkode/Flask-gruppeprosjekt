38a4dcd4a5067142019c8d8d03b75e4d61bdc5ca
vegard1903 <vesod1126@uib.no>
2024-10-24 09:45:24 +0200

    Endret portnummer

--- server.py
+++ server.py
@@ -3,6 +3,6 @@
 from project import app
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="localhost", port=9999, debug=True)
 
 #Truls Tester Git
