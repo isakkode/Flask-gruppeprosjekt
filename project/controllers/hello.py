14ef78a8cbc5f2cd0dc7bb704ca06c83e218ffca
Ostepopp7 <isakridinger@outlook.com>
2024-10-24 10:20:16 +0200

    added base.html and made some changed on the template

--- project/controllers/hello.py
+++ project/controllers/hello.py
@@ -1,26 +1,7 @@
 from project import app
 from flask import render_template, request, redirect, url_for
-from project.models.Customer import findUserByUsername
 
 #route index
 @app.route('/', methods=["GET", "POST"])
 def index():
-
-    if request.method == "POST":
-        username = request.form["username"]
-        try:
-            user = findUserByUsername(username)
-            data = {
-                "username": user.username,
-                "email": user.email
-            }
-        except err:
-            print (err)
-
-    else:
-        data = {
-            "username": "Not specified",
-            "email": "Not specified"
-        }
-    return render_template('index.html.j2', data = data)
-
+    render_template('index.html.j2')
--- server.py
+++ server.py
@@ -4,7 +4,3 @@ from project import app
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000, debug=True)
-
-
-
-"ISAK KODE TEST"
